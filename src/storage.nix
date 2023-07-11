4b80e20f59ce46a937e4678eb6931a1fe1227b0a
Garrett Myrick <garrettmyrick@gmail.com>
2023-07-10 22:01:41 -0700

    updated storage folder permissions

--- src/storage.nix
+++ src/storage.nix
@@ -19,4 +19,16 @@
       mountPoint = "/mnt/storage/media";
     };
   };
+
+  # See https://github.com/NixOS/nixpkgs/issues/72394#issuecomment-549110501
+  environment.etc."mdadm.conf".text = ''
+    MAILADDR root
+  '';
+
+  systemd.tmpfiles.rules = [
+    "d /mnt 0777 root root -"
+    "d /mnt/storage 0777 root root -"
+    "d /mnt/storage/backup 0777 root root -"
+    "d /mnt/storage/media 0777 root root -"
+  ];
 }
